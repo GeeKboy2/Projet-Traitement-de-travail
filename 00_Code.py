8fc31eea7e620c77b1eb812af716590b18318ead
Larwive <larwive@gmail.com>
2022-12-17 13:07:30 +0100

    Updates by GeekBoyboss.

--- 00_Code.py
+++ 00_Code.py
@@ -17,38 +17,61 @@ fetch = cur.fetchall()
 # print(fetch)
 donnees = pd.DataFrame(data=fetch, columns=["budget", "popularity", "runtime", "revenue", "vote_average"])
 # y =pd.Series(donnees).map({0})
-print(donnees.head(5000))
+print(donnees.head(3000))
 connection.close()
+donnees=np.squeeze(np.asarray(donnees))
+X=donnees[:,0]
+Y=donnees[:,1]
+Z=donnees[:,2]
 
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+ax.scatter(X, Y, Z, c=Z, cmap='RdYlGn', linewidth=0.5)
+plt.title("avant")
 pca = prince.PCA(
-  n_components=2,
-  n_iter=3,
-  rescale_with_mean=True,  # Centrer
+  n_components=5,
+  n_iter=10,
+  rescale_with_mean=False,  # Centrer
   rescale_with_std=True,  # Réduire
   copy=False,
   check_input=True,
   engine='auto',
-  random_state=42)
+  random_state=234)
 
 pca = pca.fit(donnees)
 pca.transform(donnees).head()  # Composantes principales
-
 pca.plot_row_coordinates(donnees)
+
+#print(donnees)
+donnees=np.squeeze(np.asarray(donnees))
+print(donnees[:,0])
+X=donnees[:,0]
+Y=donnees[:,1]
+Z=donnees[:,2]
+
+
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+
+ax.scatter(X, Y, Z, c=Z, cmap='RdYlGn', linewidth=0.5)
+plt.title("apres")
 """
 ax = pca.plot_row_coordinates( #Projection sur les composantes principales
   donnees,
   ax=None,
-  figsize=(6, 6),
+  figsize=(6, 6,6),
   x_component=0,
   y_component=1,
+  z_component=2,
   labels=None,
-  #color_labels={0: 'Setosa', 1: 'Versicolor', 2: 'Virginica'},
+  color_labels={0: 'Setosa', 1: 'Versicolor', 2: 'Virginica'},
   ellipse_outline=False,
   ellipse_fill=True,
   show_points=True
 )
 ax.legend()
 """
+plt.show()
 
 print("\nValeurs propres :")
 print(pca.eigenvalues_)  # Valeurs propres
